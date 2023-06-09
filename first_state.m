b0480320809292bae2574cd535ef09a709468db1
WSONG <mu4215@gmail.com>
2023-06-09 10:04:08 +0900

    Minor fix

--- first_state.m
+++ first_state.m
@@ -1,26 +1,27 @@
 load('dcir_fit.mat')
 
-% Variables and data definition
+% 변수 및 데이터 정의
 R1 = 12.5032;
 R2 = 24.4340;
 C = 0.00806;
-I = 0.0038;
-a = 1.5117;
+
 deltaV_exp = data(22).deltaV;
 time_exp = data(22).t;
 
-% Calculate the voltage values using the model function
-voltage_model = model_func(time_exp, R1, R2, C, I, a); % pass I as argument
+% 모델 함수를 사용하여 전압 값 계산
+voltage_model = model_func(time_exp, R1, R2, C, a);
 
-% Plot data and model results on a graph
+% 데이터와 모델 결과를 그래프로 플롯
 plot(time_exp, deltaV_exp, 'b-', time_exp, voltage_model, 'r--');
-legend('Experimental data', 'Model result');
-xlabel('Time');
-ylabel('Voltage');
-title('Experimental data and Model result');
+legend('실험 데이터', '모델 결과');
+xlabel('시간');
+ylabel('전압');
+title('실험 데이터와 모델 결과');
 
 
-% Model function definition
-function voltage = model_func(time, R1, R2, C, I, a) % include I as parameter
+% 모델 함수 정의
+function voltage = model_func(time, R1, R2, C, a)
+    I = 0.0038;
+    a = 1.5117;
     voltage = I * R1 * (R1 + R2) ./ (R1 + R2 .* exp(-a .* time ./ (R1 * C)));
 end
